e6b3276d879dfea5db8d248f876139f61147a913
GitHub Action <action@github.com>
2024-09-19 06:32:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/jasonqq/starcopilot/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jasonqq/starcopilot/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jasonqq.starcopilot.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jasonqq.starcopilot.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
